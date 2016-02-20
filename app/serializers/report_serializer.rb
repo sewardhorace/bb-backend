1a49b23640083c20fd41d0f1d574a2ead3133650
Max White <maxjwhite22@gmail.com>
2016-02-20 15:09:16 -0500

    added updated_at to report serializer

--- app/serializers/report_serializer.rb
+++ app/serializers/report_serializer.rb
@@ -1,5 +1,5 @@
 class ReportSerializer < ActiveModel::Serializer
-  attributes :title, :description, :tags, :time
+  attributes :title, :description, :tags, :time, :updated_at
   has_one :room
   has_one :user, key: :author
   has_many :students
